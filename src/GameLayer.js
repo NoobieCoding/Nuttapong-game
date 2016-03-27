1637dd2d0619ea35fdf632d5fc52dbccaef2c52a
nuttapong rojanavanich <nuttapong rojanavanich>
2016-03-27 21:00:48 +0700

    Player appears on the screen

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,8 +1,10 @@
 var GameLayer = cc.LayerColor.extend({
   init: function() {
-    this._super( new cc.Color( 127, 127, 127, 255 ) );
-    this.setPosition( new cc.Point( 0, 0 ) );
-    
+    this._super(new cc.Color( 127, 127, 127, 255));
+    this.setPosition(new cc.Point(0, 0 ));
+    this.player = new Player();
+    this.player.setPosition(new cc.Point(screenWidth / 2, startPositionY));
+    this.addChild(this.player, 1);
     return true;
   }
 });
@@ -15,3 +17,6 @@ var StartScene = cc.Scene.extend({
     this.addChild( layer );
   }
 });
+
+var screenWidth = 1920;
+var startPositionY = 100;
